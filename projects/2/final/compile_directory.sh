0dddae07f3c8f0d6ddcb36e1b368900bf5098e07
Paul Nickerson <pvnick@gmail.com>
2014-12-02 00:40:46 -0500

    deliverable input structure

---
+++ projects/2/final/compile_directory.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+invoke_pandoc() {
+	INPUT_FILE="$1"
+	OUTPUT_FILE="$2"
+	echo pandoc --include-in-header $CURR_DIR/pandocheader.tex "$INPUT_FILE" -o "$OUTPUT_FILE"
+	pandoc --include-in-header $CURR_DIR/pandocheader.tex "$INPUT_FILE" -o "$OUTPUT_FILE"
+}
+
+compile_pdf() {
+	MD_FILE="$1"
+	OUTPUT_DIR_BASE="$2"
+	STRIPPED_EXTENSION="${MD_FILE%.md}"
+	MD_FILE_DIR=$(dirname "$MD_FILE")
+	OUTPUT_DIR="$OUTPUT_DIR_BASE/$MD_FILE_DIR"
+	OUTPUT_PATH="$OUTPUT_DIR_BASE/$STRIPPED_EXTENSION.pdf"
+	INPUT_ABSOLUTE=$(readlink -f $MD_FILE)
+	echo -n "Compiling $INPUT_ABSOLUTE to $OUTPUT_PATH... "
+	mkdir -p $OUTPUT_DIR
+	echo $OUTPUT_DIR
+	[[ $? != 0 ]] && echo "fail" && echo "Unable to create output directory." && exit
+	invoke_pandoc "$INPUT_ABSOLUTE" "$OUTPUT_PATH"
+	[[ $? != 0 ]] && echo "fail" && echo "Unable to create pdf." && exit
+	echo "done"
+}
+
+compile_file() {
+	FILE="$1"
+	OUTPUT_DIR_BASE="$2"
+
+	case "$FILE" in
+	*".md")
+		compile_pdf "$FILE" "$OUTPUT_DIR_BASE"
+		;;
+	*)
+		STRIPPED_EXTENSION="${MD_FILE%.md}"
+		FILE_DIR=$(dirname "$FILE")
+		OUTPUT_DIR="$OUTPUT_DIR_BASE/$FILE_DIR"
+		OUTPUT_PATH="$OUTPUT_DIR_BASE/$FILE"
+		echo -n "Copying $FILE to $OUTPUT_PATH... "
+		mkdir -p "$OUTPUT_DIR"
+		[[ $? != 0 ]] && echo "fail" && echo "Unable to create output directory." && exit
+		cp $FILE "$OUTPUT_DIR"
+		[[ $? != 0 ]] && echo "fail" && echo "Unable to copy file." && exit
+		echo "done"
+		;;
+	esac
+}
+
+compile_physical_deliverable() {
+	DIST_DIR=$(readlink -f "$1")/
+	DIST_DIR_ESCAPED="${DIST_DIR//\//\\/}"
+	cp pandocheader.tex /tmp #physical deliverable latex file needs this in its directory
+	echo $DIST_DIR_ESCAPED
+	cat "$CURR_DIR/physical_deliverable.tex" \
+		| sed 's/\\importdeliverable{/\\importdeliverable{'$DIST_DIR_ESCAPED'/g' > /tmp/physical_deliverable_compiled.tex
+	echo "Compiling physical deliverable"
+	pdflatex "\newcommand\inputincludepath{$DIST_DIR}\input{/tmp/physical_deliverable_compiled.tex}"
+	[[ $? != 0 ]] && echo "fail" && echo "Unable to create pdf." && exit
+	echo "done"
+}
+
+compile_and_run_tests() {
+	local SOURCE_DIR="$1"
+	echo "Compiling and running tests at $SOURCE_DIR"
+	local MY_DIR=$(pwd)
+	cd "$SOURCE_DIR"
+	./compile.sh
+	./unit_tester -s > output.txt
+	cd old_tests
+	./compile.sh
+	./fuzzer | head -n 100000 > output.txt
+	cd "$MY_DIR"
+}
+
+echo "Compiling project"
+CURR_DIR=$(pwd)
+DIST_DIR=$CURR_DIR"/dist/project_1"
+rm -r $DIST_DIR 2> /dev/null
+mkdir -p $DIST_DIR
+cd input
+compile_and_run_tests "ssll/source" 
+compile_and_run_tests "cdal/source" 
+compile_and_run_tests "psll/source" 
+compile_and_run_tests "sdal/source" 
+FILES="$(find . -name '*.?*')"
+IFS=$'\n'
+for FILE in $FILES; do
+	compile_file "$FILE" "$DIST_DIR"
+done
+cd $CURR_DIR
+compile_physical_deliverable "$DIST_DIR"
+echo "Success"
+
+echo "\n\n\n\nWarning: Make sure to reenable test compile/running before turning in assignment"
---
+++ projects/2/final/input/part3/bucket/source/buckets_map.h
@@ -0,0 +1,264 @@
+#ifndef _BUCKETS_MAP_H_
+#define _BUCKETS_MAP_H_
+
+#include <iostream>
+#include "../../common/common.h"
+#include "../../common/SSLL.h"
+#include "../../common/priority_queue.h"
+
+namespace cop3530 {
+    class HashMapBuckets {
+    private:
+        typedef int key_type;
+        typedef char value_type;
+        typedef hash_utils::ClusterInventory ClusterInventory;
+        struct Item {
+            key_type key;
+            value_type value;
+            Item* next;
+            bool is_dummy;
+            Item(Item* next, key_type const& key, value_type const& value): next(next), is_dummy(false) {}
+            Item(Item* next): next(next), is_dummy(true) {}
+        };
+        struct Bucket {
+            Item* head; //use a head pointer to the first node, and include a dummy node at the end (but dont store its pointer)
+            Bucket() {
+                Item* tail = new Item(nullptr);
+                head = tail;
+            }
+            ~Bucket() {
+                while ( ! head->is_dummy) {
+                    Item* to_delete = head;
+                    head = head->next;
+                    delete to_delete;
+                }
+                delete head; //tail
+            }
+        };
+        typedef Item* link;
+        Bucket* buckets;
+        size_t num_buckets = 0;
+        size_t num_items = 0;
+        size_t hash(key_type const& key) {
+            size_t M = capacity();
+            hash_utils::functors::primary_hashes::hash_basic hasher;
+            return hasher(key) % M;
+        }
+        /*
+            searches the bucket corresponding to the specified key's hash for that
+            key. if found, stores a reference to that item and returns P, the number of
+            probe attempts needed to get to the item (ie the number of chain links needed
+            to be traversed). otherwise return -1 * P and stores the pointer to the tail dummy node in
+            item_ptr.
+        */
+        int search_internal(key_type const& key, link& item_ptr) {
+            int probe_attempts = 1;
+            size_t hash_val = hash(key);
+            Bucket& bucket = buckets[hash_val];
+            item_ptr = bucket.head;
+            while ( ! item_ptr->is_dummy) {
+                if (item_ptr->key == key) {
+                    //found the key
+                    return probe_attempts;
+                }
+                item_ptr = item_ptr->next;
+                ++probe_attempts;
+            }
+            //key not found
+            return probe_attempts * -1;
+        }
+        void init() {
+            buckets = new Bucket[num_buckets];
+            num_items = 0;
+        }
+    public:
+        HashMapBuckets(size_t const min_buckets)
+        {
+            if (min_buckets == 0) {
+                throw std::domain_error("min_buckets must be at least 1");
+            }
+            cop3530::hash_utils::functors::map_capacity_planner capacity_planner;
+            num_buckets = capacity_planner(min_buckets); //make capacity a power of 2, greater than the minimum capacity
+            init();
+        }
+        ~HashMapBuckets() {
+            delete[] buckets;
+        }
+        /*
+            if there is space available, adds the specified key/value-pair to the hash map and returns the
+            number of probes required, P; otherwise returns -1 * P (that's a lie: we will always have space
+            available because each bucket contains a linked list that is indefinitely growable). If an item
+            already exists in the map with the same key, replace its value.
+        */
+        int insert(key_type const& key, value_type const& value) {
+            Item* item;
+            int probes_required = search_internal(key, item);
+            if (probes_required > 0)
+                //found item
+                item->value = value;
+            else {
+                //currently holding tail (item not found). transform it into a valid item then add a new tail
+                item->is_dummy = false;
+                item->key = key;
+                item->value = value;
+                item->next = new Item(nullptr);
+                ++num_items;
+            }
+            return std::abs(probes_required);
+        }
+        /*
+            if there is an item matching key, removes the key/value-pair from the map, stores it's value in
+            value, and returns the number of probes required, P; otherwise returns -1 * P.
+        */
+        int remove(key_type const& key, value_type& value) {
+            Item* item;
+            int probes_required = search_internal(key, item);
+            if (probes_required > 0) {
+                //found item
+                value = item->value;
+                //swap the current item for the next one
+                Item* to_delete = item->next;
+                *item = *to_delete;
+                delete to_delete;
+                --num_items;
+            }
+            return probes_required;
+        }
+        /*
+            if there is an item matching key, stores it's value in value, and returns the
+            number of probes required, P; otherwise returns -1 * P. Regardless, the item
+            remains in the map.
+        */
+        int search(key_type const& key, value_type& value) {
+            Item* item;
+            int probes_required = search_internal(key, item);
+            if (probes_required > 0) {
+                //found item
+                value = item->value;
+            }
+            return probes_required;
+        }
+        /*
+            removes all items from the map.
+        */
+        void clear() {
+            delete buckets;
+            init();
+        }
+        /*
+            returns true IFF the map contains no elements.
+        */
+        bool is_empty() {
+            return size() == 0;
+        }
+        /*
+            returns the number of slots in the map.
+        */
+        size_t capacity() {
+            return num_buckets;
+        }
+        /*
+            returns the number of items actually stored in the map.
+        */
+        size_t size() {
+            return num_items;
+        }
+        /*
+            returns the map's load factor (size = load * capacity).
+        */
+        double load() {
+            return static_cast<double>(size()) / capacity();
+        }
+        /*
+            inserts into the ostream, the backing array's contents in sequential order.
+            Empty slots shall be denoted by a hyphen, non-empty slots by that item's
+            key. [This function will be used for debugging/monitoring].
+        */
+        std::ostream& print(std::ostream& out) {
+            size_t cap = capacity();
+            bool print_separator = false;
+            out << '[';
+            for (size_t i = 0; i != cap; ++i) {
+                Bucket const& bucket = buckets[i];
+                for (Item* item = bucket.head; item->is_dummy != false; item = item->next) {
+                    if (print_separator)
+                        out << "|";
+                    else
+                        print_separator = true;
+                    out << item->key;
+                }
+            }
+            out << ']';
+            return out;
+        }
+
+        /*
+            returns a priority queue containing cluster sizes and instances (in the form of ClusterInventory
+            struct instances), sorted by cluster size.
+        */
+        priority_queue<ClusterInventory> cluster_distribution() {
+            //use a simple linked list to count cluster instances, then feed those to a priority queue and return it.
+            priority_queue<ClusterInventory> cluster_pq;
+            if (size() == 0) return cluster_pq;
+            SSLL<ClusterInventory> clusters;
+            size_t M = capacity();
+            for (size_t i = 0; i != M; ++i) {
+                Bucket const& bucket = buckets[i];
+                size_t bucket_size = 0;
+                Item* item_ptr = bucket.head;
+                while ( ! item_ptr->is_dummy) {
+                    ++bucket_size;
+                    item_ptr = item_ptr->next;
+                }
+                //I don't love this O(N^2) implementation, but premature optimization is the root of all evil and late projects
+                SSLL<ClusterInventory>::iterator cluster_iterator = clusters.begin();
+                SSLL<ClusterInventory>::iterator cluster_iterator_end = clusters.end();
+                bool found_cluster = false;
+                for (; cluster_iterator != cluster_iterator_end; ++cluster_iterator) {
+                    if (cluster_iterator->cluster_size == bucket_size) {
+                        found_cluster = true;
+                        break;
+                    }
+                }
+                if (found_cluster)
+                    cluster_iterator->num_instances++;
+                else
+                    clusters.push_back({bucket_size, 1});
+            }
+            SSLL<ClusterInventory>::const_iterator cluster_iterator = clusters.begin();
+            SSLL<ClusterInventory>::const_iterator cluster_iterator_end = clusters.end();
+            for (; cluster_iterator != cluster_iterator_end; ++cluster_iterator) {
+                if (cluster_iterator->cluster_size > 0)
+                    cluster_pq.add_to_queue(*cluster_iterator);
+            }
+            return cluster_pq;
+        }
+
+        /*
+            generate a random number, R, (1,size), and starting with slot zero in the backing array,
+            find the R-th occupied slot; remove the item from that slot (adjusting subsequent items as
+            necessary), and return its key.
+        */
+        key_type remove_random() {
+            if (size() == 0) throw std::logic_error("Cant remove from an empty map");
+            size_t num_slots = capacity();
+            size_t ith_node_to_delete = 1 + hash_utils::rand_i(size());
+            for (size_t i = 0; i != num_slots; ++i) {
+                Bucket const& bucket = buckets[i];
+                Item* item_ptr = bucket.head;
+                while ( ! item_ptr->is_dummy) {
+                    if (--ith_node_to_delete == 0) {
+                        key_type key = item_ptr->key;
+                        value_type val_dummy;
+                        remove(key, val_dummy);
+                        return key;
+                    }
+                    item_ptr = item_ptr->next;
+                }
+            }
+            throw std::logic_error("Unexpected end of remove_random function");
+        }
+    };
+}
+
+#endif
